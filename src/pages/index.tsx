18493d3f88afd1b470569eec6a3d1d7d15a0e64b
Bruna Schneiders <bruna.schneiders@gmail.com>
2021-05-23 11:30:42 -0300

    create home page and global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+import Head from 'next/head';
 import { GetStaticProps } from 'next';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +26,48 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({}: HomeProps) {
+  return (
+    <>
+      <Head>Home | spacetravelling</Head>
+
+      <main className={commonStyles.container}>
+        <div className={styles.posts}>
+          <a href="#">
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div>
+              <time>
+                <FiCalendar />
+                15 Mar 2021
+              </time>
+              <span>
+                <FiUser />
+                Joseph Oliveira
+              </span>
+            </div>
+          </a>
+          <a href="#">
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div>
+              <time>
+                <FiCalendar />
+                15 Mar 2021
+              </time>
+              <span>
+                <FiUser />
+                Joseph Oliveira
+              </span>
+            </div>
+          </a>
+
+          <button type="button"> Carregar mais posts</button>
+        </div>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
